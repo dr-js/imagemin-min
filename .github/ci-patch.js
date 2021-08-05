6ca8e930725d03a640a8432ae50861658d6a79e1
dr-js <dr@dr.run>
2021-08-05 21:17:38 +0800

    CHG: ci: code sort

--- .github/ci-patch.js
+++ .github/ci-patch.js
@@ -1,36 +1,13 @@
-const { resolve } = require('path')
-const { release, arch } = require('os')
-const { run } = require('@dr-js/core/library/node/system/Run')
+const { runMain, commonInfoPatchCombo } = require('@dr-js/dev/library/main.js')
 
-console.log(`[ci-patch] system: ${process.platform}-${release()}[${arch()}]`)
-console.log(`[ci-patch] node: ${process.version}`)
-console.log(`[ci-patch] with @dr-js/core@${require('@dr-js/core/package.json').version}`)
+runMain(async (logger) => {
+  commonInfoPatchCombo(logger)
 
-const PATH_ROOT = resolve(__dirname, '../')
-console.log(`[ci-patch] PATH_ROOT: ${PATH_ROOT}`)
+  // const { RUN, fromHome, config: { COMMAND_SUDO_NPM } } = commonInfoPatchCombo(logger)
 
-const quickRun = async (argListOrString) => { // accept string list of very basic command do not need extra quote
-  const argList = Array.isArray(argListOrString) ? argListOrString : argListOrString.split(' ').filter(Boolean)
-  const command = argList.shift()
-  console.log(`[ci-patch] run: "${command} ${argList.join(' ')}"`)
-  await run({ command, argList, option: { cwd: PATH_ROOT }, describeError: true }).promise
-}
+  // logger.padLog('Patch npm cache path') // set cache path to `~/.npm/` for all platform (only win32 for now)
+  // RUN([ ...`${COMMAND_SUDO_NPM} config --global set cache`.split(' '), fromHome('.npm/') ])
 
-const main = async () => {
-  const IS_WIN32 = process.platform === 'win32'
-
-  // Patch git
-  //   fix win32 CI cause `something to commit` test error: https://github.com/actions/checkout/issues/135#issuecomment-602171132
-  IS_WIN32 && await quickRun('git config core.autocrlf false')
-  IS_WIN32 && await quickRun('git config core.eol lf')
-  IS_WIN32 && await quickRun('git rm --cached -r .') // reset Git index, `rm .git/index` also work, check: https://stackoverflow.com/questions/5787937/git-status-shows-files-as-changed-even-though-contents-are-the-same/41041699#41041699
-  IS_WIN32 && await quickRun('git reset --hard')
-}
-
-main().then(
-  () => console.log('[ci-patch] done'),
-  (error) => {
-    console.error('[ci-patch] error:', error)
-    process.exitCode = 1
-  }
-)
+  // logger.padLog('Patch install "@dr-js/dev" globally')
+  // RUN(`${COMMAND_SUDO_NPM} install --global @dr-js/dev@0.4`)
+}, 'ci-patch')
