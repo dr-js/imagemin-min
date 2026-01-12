7686d330fa8d0cbffb50a7afb4bbd5d5ecfdc326
dr-js <dr@dr.run>
2026-01-12 02:53:20 +0000

    ADD: `@min-pack/sharp-x86-64-v1`

--- source/battery/sharp.test.js
+++ source/battery/sharp.test.js
@@ -1,7 +1,7 @@
 import { doThrowAsync } from '@dr-js/core/module/common/verify.js'
 import { PATH_TEST_BUFFER_JPG, PATH_TEST_BUFFER_PNG, PATH_TEST_BUFFER_GIF, PATH_TEST_BUFFER_SVG, PATH_TEST_BUFFER_WEBP, PATH_TEST_BUFFER_NON_IMAGE } from './common.test/function.js'
 
-import { configMinPackSharp } from './sharp.js'
+import { configMinPackSharp, loadSharpDefault, loadSharpFallback } from './sharp.js'
 import { createStepper } from '@dr-js/core/module/common/time'
 import { readBufferSync } from '@dr-js/core/module/node/fs/File'
 import { binary, time } from '@dr-js/core/module/common/format'
@@ -11,10 +11,11 @@ const { describe, it, info = console.log } = global
 const testSharpAsync = async ({
   TEST_TAG,
   SOURCE_BUFFER, PATH_SOURCE_BUFFER,
+  LOAD_SHARP,
   stepper = createStepper()
 }) => {
   if (!SOURCE_BUFFER) SOURCE_BUFFER = readBufferSync(PATH_SOURCE_BUFFER)
-  const { getImgMeta, processImg } = configMinPackSharp()
+  const { getImgMeta, processImg } = configMinPackSharp({ __SHARP_OVERRIDE__: await LOAD_SHARP() })
   info(`[${TEST_TAG}] done prepare (+${time(stepper())})`)
 
   const imgMeta = await getImgMeta(SOURCE_BUFFER)
@@ -28,11 +29,21 @@ const testSharpAsync = async ({
 }
 
 describe('Battery|sharp', () => {
-  it('png', async () => testSharpAsync({ TEST_TAG: 'sharp-png', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_PNG }))
-  it('jpg', async () => testSharpAsync({ TEST_TAG: 'sharp-jpg', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_JPG }))
-  it('webp', async () => testSharpAsync({ TEST_TAG: 'sharp-webp', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_WEBP }))
-  it('gif', async () => testSharpAsync({ TEST_TAG: 'sharp-gif', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_GIF }))
-  it('svg', async () => testSharpAsync({ TEST_TAG: 'sharp-svg', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_SVG }))
-  // it('non-img', () => testSharpAsync({ TEST_TAG: 'sharp-non-img', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_NON_IMAGE }))
-  it('non-img', () => doThrowAsync(async () => testSharpAsync({ TEST_TAG: 'sharp-non-img', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_NON_IMAGE })))
+  it('png', async () => testSharpAsync({ TEST_TAG: 'sharp-png', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_PNG, LOAD_SHARP: loadSharpDefault }))
+  it('jpg', async () => testSharpAsync({ TEST_TAG: 'sharp-jpg', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_JPG, LOAD_SHARP: loadSharpDefault }))
+  it('webp', async () => testSharpAsync({ TEST_TAG: 'sharp-webp', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_WEBP, LOAD_SHARP: loadSharpDefault }))
+  it('gif', async () => testSharpAsync({ TEST_TAG: 'sharp-gif', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_GIF, LOAD_SHARP: loadSharpDefault }))
+  it('svg', async () => testSharpAsync({ TEST_TAG: 'sharp-svg', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_SVG, LOAD_SHARP: loadSharpDefault }))
+  // it('non-img', () => testSharpAsync({ TEST_TAG: 'sharp-non-img', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_NON_IMAGE, LOAD_SHARP: loadSharpDefault }))
+  it('non-img', () => doThrowAsync(async () => testSharpAsync({ TEST_TAG: 'sharp-non-img', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_NON_IMAGE, LOAD_SHARP: loadSharpDefault })))
+})
+
+process.arch === 'x64' && describe('Battery|sharp(x86-64-v1)', () => {
+  it('png', async () => testSharpAsync({ TEST_TAG: 'sharp-png', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_PNG, LOAD_SHARP: loadSharpFallback }))
+  it('jpg', async () => testSharpAsync({ TEST_TAG: 'sharp-jpg', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_JPG, LOAD_SHARP: loadSharpFallback }))
+  it('webp', async () => testSharpAsync({ TEST_TAG: 'sharp-webp', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_WEBP, LOAD_SHARP: loadSharpFallback }))
+  it('gif', async () => testSharpAsync({ TEST_TAG: 'sharp-gif', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_GIF, LOAD_SHARP: loadSharpFallback }))
+  it('svg', async () => testSharpAsync({ TEST_TAG: 'sharp-svg', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_SVG, LOAD_SHARP: loadSharpFallback }))
+  // it('non-img', () => testSharpAsync({ TEST_TAG: 'sharp-non-img', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_NON_IMAGE, LOAD_SHARP: loadSharpFallback }))
+  it('non-img', () => doThrowAsync(async () => testSharpAsync({ TEST_TAG: 'sharp-non-img', PATH_SOURCE_BUFFER: PATH_TEST_BUFFER_NON_IMAGE, LOAD_SHARP: loadSharpFallback })))
 })
